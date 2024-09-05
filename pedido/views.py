e2fa1673b96914848084b22ed4103a6e1cef1806
Estuart <estuartmatheus11@gmail.com>
2024-09-05 11:52:10 -0300

    Atualização 05/09/2024

---
+++ pedido/views.py
@@ -0,0 +1,183 @@
+from django.shortcuts import redirect, reverse
+from django.views.generic import ListView, DetailView
+from django.views import View
+# from django.http import HttpResponse
+from django.contrib import messages
+
+from .models import Pedido, ItemPedido
+from produto.models import Variacao
+
+from utils import utils
+
+
+class DispatchLoginRequiredMixin(View):
+    def dispatch(self, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return redirect('perfil:criar')
+
+        return super().dispatch(*args, **kwargs)
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(usuario=self.request.user)
+        return qs
+
+
+class Pagar(DispatchLoginRequiredMixin, DetailView):
+    template_name = 'pedido/pagar.html'
+    model = Pedido
+    pk_url_kwarg = 'pk'
+    context_object_name = 'pedido'
+
+class Pagar(DispatchLoginRequiredMixin, DetailView):
+    template_name = 'pedido/pagar.html'
+    model = Pedido
+    pk_url_kwarg = 'pk'
+    context_object_name = 'pedido'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pedido = self.object
+        
+        # Criar a mensagem
+        itens_pedido = pedido.itempedido_set.all()
+        mensagem = "Olá! Gostaria de realizar o pagamento do \n\n"
+
+        for item in itens_pedido:
+            mensagem += f"pedido N°.*{pedido.id}*\n"
+
+        
+        # subtotal = 100.00  # Corrigido para float
+        # frete = 20.00      # Corrigido para float
+        # total = 1000.00    # Corrigido para float
+        
+        # mensagem += f"📦 Subtotal: R$ {subtotal:.2f}\n"
+        # mensagem += f"🚚 Frete: R$ {frete:.2f} (se aplicável)\n"
+        # mensagem += f"💵 Total a Pagar: R$ {total:.2f}\n"
+
+        # Criar o link do WhatsApp
+        numero_destino = '5543996341638'  # Substitua pelo número de destino
+        mensagem_url = f"https://wa.me/{numero_destino}?text={mensagem.replace(' ', '%20')}"
+
+        # Adiciona o link ao contexto para uso no template
+        context['whatsapp_link'] = mensagem_url
+        return context
+
+
+
+    # subtotal = sum(item.preco * item.quantidade for item in itens_pedido)
+
+    
+    
+    # mensagem_url = f"https://wa.me/{numero_destino}?text={mensagem.replace(' ', '%20').replace('\n', '%0A')}"
+
+
+
+class SalvarPedido(View):
+    template_name = 'pedido/pagar.html'
+
+    def get(self, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            messages.error(
+                self.request,
+                'Você precisa fazer login.'
+            )
+            return redirect('perfil:criar')
+
+        if not self.request.session.get('carrinho'):
+            messages.error(
+                self.request,
+                'Seu carrinho está vazio.'
+            )
+            return redirect('produto:lista')
+
+        carrinho = self.request.session.get('carrinho')
+        carrinho_variacao_ids = [v for v in carrinho]
+        bd_variacoes = list(
+            Variacao.objects.select_related('produto')
+            .filter(id__in=carrinho_variacao_ids)
+        )
+
+        for variacao in bd_variacoes:
+            vid = str(variacao.id)
+
+            estoque = variacao.estoque
+            qtd_carrinho = carrinho[vid]['quantidade']
+            preco_unt = carrinho[vid]['preco_unitario']
+            preco_unt_promo = carrinho[vid]['preco_unitario_promocional']
+
+            error_msg_estoque = ''
+
+            if estoque < qtd_carrinho:
+                carrinho[vid]['quantidade'] = estoque
+                carrinho[vid]['preco_quantitativo'] = estoque * preco_unt
+                carrinho[vid]['preco_quantitativo_promocional'] = estoque * \
+                    preco_unt_promo
+
+                error_msg_estoque = 'Estoque insuficiente para alguns '\
+                    'produtos do seu carrinho. '\
+                    'Reduzimos a quantidade desses produtos. Por favor, '\
+                    'verifique quais produtos foram afetados a seguir.'
+
+            if error_msg_estoque:
+                messages.error(
+                    self.request,
+                    error_msg_estoque
+                )
+
+                self.request.session.save()
+                return redirect('produto:carrinho')
+
+        qtd_total_carrinho = utils.cart_total_qtd(carrinho)
+        valor_total_carrinho = utils.cart_totals(carrinho)
+
+        pedido = Pedido(
+            usuario=self.request.user,
+            total=valor_total_carrinho,
+            qtd_total=qtd_total_carrinho,
+            status='C',
+        )
+
+        pedido.save()
+
+        ItemPedido.objects.bulk_create(
+            [
+                ItemPedido(
+                    pedido=pedido,
+                    produto=v['produto_nome'],
+                    produto_id=v['produto_id'],
+                    variacao=v['variacao_nome'],
+                    variacao_id=v['variacao_id'],
+                    preco=v['preco_quantitativo'],
+                    preco_promocional=v['preco_quantitativo_promocional'],
+                    quantidade=v['quantidade'],
+                    imagem=v['imagem'],
+                ) for v in carrinho.values()
+            ]
+        )
+
+        del self.request.session['carrinho']
+
+        return redirect(
+            reverse(
+                'pedido:pagar',
+                kwargs={
+                    'pk': pedido.pk
+                }
+            )
+        )
+
+
+class Detalhe(DispatchLoginRequiredMixin, DetailView):
+    model = Pedido
+    context_object_name = 'pedido'
+    template_name = 'pedido/detalhe.html'
+    pk_url_kwarg = 'pk'
+
+
+class Lista(DispatchLoginRequiredMixin, ListView):
+    model = Pedido
+    context_object_name = 'pedidos'
+    template_name = 'pedido/lista.html'
+    paginate_by = 10
+    ordering = ['-id']
