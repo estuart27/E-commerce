01f25ef22a7a11ce42ca37d9769052751b8dc391
Hub Esilvestre <estuartmatheus11@gmail.com>
2025-04-01 21:50:50 -0300

    Atualizando-01/04/2025

--- produto/views.py
+++ produto/views.py
@@ -101,91 +101,87 @@ class DetalheProduto(DetailView):
     context_object_name = 'produto'
     slug_url_kwarg = 'slug'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        produto_atual = self.get_object()
+        
+        # Busca 4 produtos quaisquer (excluindo o produto atual)
+        context['produtos'] = models.Produto.objects.exclude(
+            id=produto_atual.id
+        )[:4]  # Limita a 4 produtos
+        
+        context['categories'] = Category.objects.all()
+        return context
+
 
 class AdicionarAoCarrinho(View):
     def get(self, *args, **kwargs):
-        http_referer = self.request.META.get(
-            'HTTP_REFERER',
-            reverse('produto:lista')
-        )
-        variacao_id = self.request.GET.get('vid')
+        request = self.request
+        http_referer = request.META.get('HTTP_REFERER', reverse('produto:lista'))
+        variacao_id = request.GET.get('vid')
+
+        # Validação da quantidade
+        try:
+            quantidade = int(request.GET.get('quantidade', 1))
+            if quantidade < 1:
+                messages.error(request, 'A quantidade deve ser maior que zero.')
+                return redirect(http_referer)
+        except ValueError:
+            messages.error(request, 'Quantidade inválida.')
+            return redirect(http_referer)
 
         if not variacao_id:
-            messages.error(
-                self.request,
-                'Produto não existe'
-            )
+            messages.error(request, 'Produto não existe')
             return redirect(http_referer)
 
         variacao = get_object_or_404(models.Variacao, id=variacao_id)
         variacao_estoque = variacao.estoque
         produto = variacao.produto
 
-        produto_id = produto.id
-        produto_nome = produto.nome
-        variacao_nome = variacao.nome or ''
-        preco_unitario = variacao.preco
-        preco_unitario_promocional = variacao.preco_promocional
-        quantidade = 1
-        slug = produto.slug
-        imagem = produto.imagem
-
-        if imagem:
-            imagem = imagem.name
-        else:
-            imagem = ''
-
         if variacao.estoque < 1:
-            messages.error(
-                self.request,
-                'Estoque insuficiente'
-            )
+            messages.error(request, 'Estoque insuficiente')
             return redirect(http_referer)
 
-        if not self.request.session.get('carrinho'):
-            self.request.session['carrinho'] = {}
-            self.request.session.save()
+        if not request.session.get('carrinho'):
+            request.session['carrinho'] = {}
+            request.session.save()
 
-        carrinho = self.request.session['carrinho']
+        carrinho = request.session['carrinho']
 
         if variacao_id in carrinho:
-            quantidade_carrinho = carrinho[variacao_id]['quantidade']
-            quantidade_carrinho += 1
+            quantidade_carrinho = carrinho[variacao_id]['quantidade'] + quantidade
 
             if variacao_estoque < quantidade_carrinho:
                 messages.warning(
-                    self.request,
-                    f'Estoque insuficiente para {quantidade_carrinho}x no '
-                    f'produto "{produto_nome}". Adicionamos {variacao_estoque}x '
-                    f'no seu carrinho.'
+                    request,
+                    f'Estoque insuficiente para {quantidade_carrinho}x do produto "{produto.nome}". '
+                    f'Adicionamos {variacao_estoque}x no seu carrinho.'
                 )
                 quantidade_carrinho = variacao_estoque
 
             carrinho[variacao_id]['quantidade'] = quantidade_carrinho
-            carrinho[variacao_id]['preco_quantitativo'] = preco_unitario * \
-                quantidade_carrinho
-            carrinho[variacao_id]['preco_quantitativo_promocional'] = preco_unitario_promocional * \
-                quantidade_carrinho
+            carrinho[variacao_id]['preco_quantitativo'] = variacao.preco * quantidade_carrinho
+            carrinho[variacao_id]['preco_quantitativo_promocional'] = variacao.preco_promocional * quantidade_carrinho
         else:
             carrinho[variacao_id] = {
-                'produto_id': produto_id,
-                'produto_nome': produto_nome,
-                'variacao_nome': variacao_nome,
+                'produto_id': produto.id,
+                'produto_nome': produto.nome,
+                'variacao_nome': variacao.nome or '',
                 'variacao_id': variacao_id,
-                'preco_unitario': preco_unitario,
-                'preco_unitario_promocional': preco_unitario_promocional,
-                'preco_quantitativo': preco_unitario,
-                'preco_quantitativo_promocional': preco_unitario_promocional,
-                'quantidade': 1,
-                'slug': slug,
-                'imagem': imagem,
+                'preco_unitario': variacao.preco,
+                'preco_unitario_promocional': variacao.preco_promocional,
+                'preco_quantitativo': variacao.preco * quantidade,
+                'preco_quantitativo_promocional': variacao.preco_promocional * quantidade,
+                'quantidade': quantidade,
+                'slug': produto.slug,
+                'imagem': produto.imagem.name if produto.imagem else '',
             }
 
-        self.request.session.save()
+        request.session.save()
 
         messages.success(
-            self.request,
-            f'Produto {produto_nome} {variacao_nome} adicionado ao seu '
+            request,
+            f'Produto {produto.nome} {variacao.nome} adicionado ao seu '
             f'carrinho {carrinho[variacao_id]["quantidade"]}x.'
         )
 
